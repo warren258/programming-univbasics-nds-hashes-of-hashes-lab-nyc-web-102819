8e8a9a732ed7bd0ab84c3221b607b7b74277be09
Warren McDonald <warrenjmac@mail.com>
2019-10-22 17:30:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,7 +43,17 @@ def naming_system
   # HoH all the way to Species!
 #Kingdom = {Phylum = {}, Class = {}, Order = {}, Family = {}, Genus = {}, #Species = {}}
 {label: "Kingdom",
-sub_category: {label: "Phylum"
+sub_category: {label: "Phylum",
+sub_category: {label: "Class",
+sub_category: {label: "Order",
+sub_category: {label: "Family",
+sub_category: {label: "Genus",
+sub_category: {label: "Species"}
+}
+}
+}
+}
+}
 }
 end
 
